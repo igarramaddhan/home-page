c63618dbea01f1cb4a5352cca484d79336f7f461
Igar Ramaddhan <igar.ramaddhan@kumparan.com>
2020-05-23 21:35:33 +0700

    add message

--- src/pages/Greet.js
+++ src/pages/Greet.js
@@ -1,10 +1,44 @@
 // @flow
 import React from 'react';
+import styled from 'styled-components';
 import {useParams} from 'react-router-dom';
+
+const Container = styled.div`
+  flex: 1;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  background-color: #232933;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  height: 100vh;
+  max-width: 850px;
+  margin: auto;
+  font-size: 22px;
+  flex-direction: column;
+  justify-content: center;
+`;
 
 const Greet = () => {
   const {name} = useParams<{name: string}>();
-  return <div>Hellow {atob(name)}</div>;
+  return (
+    <Container>
+      <ContentContainer>
+        <p>{`Assalamu'alaikum, ${atob(name)}`}</p>
+        <p>Semoga kamu sehat selalu dan selalu dalam lindungan-Nya.</p>
+        <p>
+          Mohon maaf lahir dan batin ya kalo aku ada salah selama ini. <br />
+          Maaf belum bisa ngasih apa-apa dulu ya hehe 😁. <br />
+          Maaf kalo juga sering ngerepotin 🙏
+        </p>
+        <p>Selamat Hari Raya Idul Fitri 1441 H</p>
+        <p>{`Wassalamu'alaikum Wr. Wb.`}</p>
+        <p>Salam, Igar</p>
+      </ContentContainer>
+    </Container>
+  );
 };
 
 export default Greet;
